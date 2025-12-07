95e3e6d665a2f0728a487ca642fa6166859e4265
martinbasa-stack <martin.basa@gmail.com>
2025-12-07 11:30:32 -0300

    Delete src directory

--- src/trades/__init__.py
+++
@@ -1,5 +0,0 @@
-from .models import Trade, TradeTable
-from .manager import TradeManager
-from src.settings import strategies_class
-
-trade_manager_class = TradeManager(get_strategies_ids_func=strategies_class.id_list, get_strategy_by_id_func=strategies_class.get_by_id)
