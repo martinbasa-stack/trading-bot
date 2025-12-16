c6fd8ea0205539af0854e00ab13e4f86c5e9aad3
martinbasa-stack <martin.basa@gmail.com>
2025-12-16 15:29:54 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -13,11 +13,11 @@ logging.basicConfig(level=logging.INFO,
                     filemode="a", # a-> Append; w-> Clear and start from 0
                     format='%(asctime)s - %(levelname)s - %(message)s')
 
-import src #use only to call functions
-from src import  settings_obj, strategy_run
+from src import  settings_obj, strategy_run, shut_down, bp
 from src.strategy import asset_manager_obj
 from src.telegram import send_telegram_msg
-from src.binance import stream_manager_obj, stream_main,  ws_manager_obj, websocet_main #ping_websocet, fetch_exchange_info, fetch_user_data
+from src.market_history import history_run
+from src.binance import stream_manager_obj, stream_main,  ws_manager_obj, websocet_main
 
 import threading
 import atexit
@@ -31,7 +31,7 @@ from flask_simplelogin import SimpleLogin
 #---------------------SYSTEM -----------------------------
 # Configure logger
 # Create a logger for this module
-logger = logging.getLogger(__name__)
+logger = logging.getLogger("app")
 logger.setLevel(logging.INFO)
 
 # Prevent propagation to the root logger
@@ -53,7 +53,7 @@ time_ping = 0
 def cleanup_function():
     logger.warning("Performing cleanup operations before exiting...") 
     print("Performing cleanup operations before exiting...") 
-    src.shut_down()
+    shut_down()
     threads_shutdown_event.set()
     stream_manager_obj.disconnect()
     ws_manager_obj.disconnect()
@@ -71,52 +71,87 @@ app.config['SECRET_KEY'] = b'_5#y2L"F4Q8z\n\xec]/' # A secret key is required fo
 app.config['SIMPLELOGIN_USERNAME'] = settings_obj.get("user")
 app.config['SIMPLELOGIN_PASSWORD'] = settings_obj.get("password")
 SimpleLogin(app)# Register the blueprint containing the routes
-app.register_blueprint(src.bp)
+app.register_blueprint(bp)
 
 # Disable Werkzeug request logging
 log = logging.getLogger('werkzeug')
 log.disabled = True 
 
 #---------------------main task for data pull and strategy-----------------
-def _main():
+def main():
     first_run = True 
+    report = False
     consol_update_count = 0
+    start = _now()
+
     while True:            
         # Main thread activity 
         loop_update = int(settings_obj.get("strategyUpdate"))
         if first_run:           
             logger.info("First run start")
             print("First run start")
-        i=0
-        step = 3
-        data_current_time = 180
-        run_loop = not first_run and (not stream_manager_obj.all_streams_available() or not stream_manager_obj.all_data_current(data_current_time))
-        while run_loop:
-            if stream_manager_obj.all_streams_available() and stream_manager_obj.all_data_current(data_current_time):
-                run_loop = False
-            print(f"Wait for all streams to be available.")
-            print(f"Waiting: {i} s ")
-            print(f"Current active streams: {stream_manager_obj.get_active_list()}")
-            consol_update_count = 0
-            time.sleep(step)
-            i +=step
+
+        if not first_run:
+            _stream_data_monitor(180) #While loop until stream data is ok
         
         last_ping = ws_manager_obj.get_last_ping_resp()
 
         # Write status to console
-        consol_update_count +=loop_update
-        if consol_update_count > 3600 * 1.5 or first_run:
+        if consol_update_count > 3600 * int(settings_obj.get("statusUpdate")) or consol_update_count == 0:
             consol_update_count = 0
-            print(f"Current active streams: {stream_manager_obj.get_active_list()}")
-            print(f"Last ping: {last_ping}")
+            update_msg = (
+                   f"Websocket connected: {ws_manager_obj.is_connected()}"
+                   f"\n     Last ping: {last_ping}"
+                   f"\n Stream connected: {ws_manager_obj.is_connected()}"
+                   f"\n     Oldest stream: {stream_manager_obj.oldest()} s, since {datetime.fromtimestamp(stream_manager_obj.oldest_timestamp())}"
+                   f"\n     Current active streams: {stream_manager_obj.get_active_list()}"
+                   )            
+            print(f"Status update: {datetime.fromtimestamp(_now())} \n "
+                  f"{update_msg}")
+            logger.info(update_msg)
 
-        if ws_manager_obj.is_connected():# and stream_data_class.all_streams_available(): #check for websocet connection status
+        consol_update_count +=loop_update        
+        
+        if ws_manager_obj.is_connected():#check for websocet connection status
+            history_run() # Call historycal data
             strategy_run() #Call main function
+            if report:
+                consol_update_count = 0
+                report = False
+                now_seconds= _now()
+                send_telegram_msg("WebSocket connection was lost!"                                  
+                              f"\n Error time {datetime.fromtimestamp(start)} "
+                              f"\n No data for {timedelta(seconds = now_seconds - start)}"
+                              f"\n Reconnection SUCCESFUL.")
         else:
-            print("No WebSocket connection") 
+            if not report:
+                consol_update_count= 0                
+            report = True
+            start = _now()
             time.sleep(5)
         time.sleep(loop_update) #Will sleap for strategy update time            
         first_run = False
+
+def _stream_data_monitor(data_current_time):
+    i=0
+    step = 3
+    run_loop = not stream_manager_obj.all_streams_available() or not stream_manager_obj.all_data_current(data_current_time)
+    start = _now()
+    while run_loop:
+        if stream_manager_obj.all_streams_available() and stream_manager_obj.all_data_current(data_current_time):
+            run_loop = False
+            now_seconds = _now()
+            send_telegram_msg(f"Stream connection was lost!"
+                              f"\n Error time {datetime.fromtimestamp(start)}"
+                              f"\n No data for {timedelta(seconds = now_seconds - start)}"
+                              " \n Reconnection SUCCESFUL. ")
+
+        print(f"Wait for all streams to be available.")
+        print(f"Waiting: {i} s ")
+        print(f"Current active streams: {stream_manager_obj.get_active_list()}")
+        time.sleep(step)
+        i +=step
+
 
 # Initialization -----------------------------------------------------
 def _initialize():
@@ -131,13 +166,16 @@ def _initialize():
         time.sleep(2)
         i=0        
         logger.info(f"initialize() Wait for connection with Websocet API")
+        print(f"initialize() Wait for connection with Websocet API")
         while not ws_manager_obj.is_connected():
             print(f"initialize() Waiting: {i} s ")
             time.sleep(1)
             i +=1        
-        logger.info(f"initialize() Waited: {i} s ")
 
-        time.sleep(3)
+        logger.info(f"initialize() Waited: {i} s ")
+        msg = (f"initialize \n Waited {i} s for connection with Websocet API \n")
+
+        time.sleep(1)
         #Get exchange info
         logger.info("Get exchange info")
         print("initialize() Get exchange info")
@@ -145,12 +183,9 @@ def _initialize():
 
         #Read user data     
         time.sleep(1)
-        if ws_manager_obj.is_connected():
-            logger.info("initialize() Read user data")   
-            print("initialize() Read user data") 
-            asset_manager_obj.update(ws_manager_obj.fetch_user_data())
-        else:
-            print("initialize() No connection") 
+        logger.info("initialize() Read user data")   
+        print("initialize() Read user data") 
+        asset_manager_obj.update(ws_manager_obj.fetch_user_data())
 
         i=0
         run_loop = True
@@ -164,12 +199,20 @@ def _initialize():
             print(f"Current active streams: {stream_manager_obj.get_active_list()}")
             time.sleep(1)
             i +=1            
+            
         logger.info(f"initialize() Waited: {i} s ")
+        msg = (f"{msg}"
+            f"Waited {i} s for all streams to be available \n"
+             f"Finished startup at: {datetime.fromtimestamp(_now())}")
         
-        send_telegram_msg("Bot startup finished")
+        send_telegram_msg(msg)
 
     except Exception as e: 
         logger.error(f"initialize() error: {e}")
+
+def _now():        
+    now_utc = datetime.now(timezone.utc)
+    return int(now_utc.timestamp())     
 
 #---------------------Streaming task Thread -----LOOP2------------------
 def thread_stream():
@@ -233,7 +276,8 @@ def thread_flaskApp():
     app.root_path = BASE_DIR
     logger.info("thread_flaskApp() Thread Stopped")
     print("thread_flaskApp() Thread Stopped")
-#Main task running forever and 3 threds one for Flask and one for Stream and one for Websocet
+
+#Main task running forever and 3 threds one for Flask, one for Stream and one for Websocet
 if __name__ == "__main__":        
     # Create and start the background task threads
     # Create and start the Flask thread
@@ -256,7 +300,7 @@ if __name__ == "__main__":
 
  # The main thread can continue to run or wait
     try:
-        _main()
+        main()
     except KeyboardInterrupt:
         print(f"Program terminated by user...")
     except Exception as e:
